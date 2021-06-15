ff3dc6838bff4292fab7eb1b976173a695eb80bf
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-06-15 12:28:08 +0200

    form for plants characteristics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  render() {
+    return (
+      <Container fluid className="container">
+        <Header as='h2'>My beautiful plants &#60;3</Header>
+        <Form className="form">
+          <Form.Field>
+            <label>Scientific Name</label>
+            <input />
+          </Form.Field>
+          <Form.Field>
+            <label>Popular Name</label>
+            <input />
+          </Form.Field>
+          <Form.Field>
+            <label>Bright light</label>
+            <input />
+          </Form.Field>
+          <Form.Field>
+            <label>Watering</label>
+            <input />
+          </Form.Field>
 
-export default App;
+          <Button color="blue" type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    )
+  }
+}
